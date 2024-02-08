d9d8884dc518d5182d60f6e6b4f30ca7ab5ec786
Cordeiro Luis <cordeiroluisff@gmail.com>
2024-02-08 09:33:00 +0100

    Implementation of the save and delete notes function

--- nlw-expert-notes/src/components/new-note-card.tsx
+++ nlw-expert-notes/src/components/new-note-card.tsx
@@ -1,11 +1,13 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import { X } from 'lucide-react';
-import { ChangeEvent, Context, FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import { toast } from 'sonner';
 
 interface NewNoteCardProps {
   onNoteCreated: (context: string) => void
 }
+
+let speechRecognition: SpeechRecognition | null = null
 
 export function NewNoteCard({ onNoteCreated} : NewNoteCardProps) {
 
@@ -27,6 +29,10 @@ export function NewNoteCard({ onNoteCreated} : NewNoteCardProps) {
   function handleSaveNote(event: FormEvent) {
     event.preventDefault();
 
+    if(contet === '') {
+      return 
+    }
+
     onNoteCreated(contet);
     
     setContent('');
@@ -36,11 +42,42 @@ export function NewNoteCard({ onNoteCreated} : NewNoteCardProps) {
   }
 
   function handleStartRecording() {
+    const isSpeechRecognitionAPIAvailable = 'SpeechRecognition' in window
+    || 'webkitSpeechRecognition' in window
+
+    if(!isSpeechRecognitionAPIAvailable) {
+      alert('Infelizmente seu navegador não suporta API de navegação ')
+      return 
+    }
     setIsRecording(true)
+    setShouldShowOnBoarding(false)
+    const SpeechRecognitionAPI = window.SpeechRecognition || window.webkitSpeechRecognition
+
+    speechRecognition = new SpeechRecognitionAPI()
+    speechRecognition.lang = 'pt-BR'
+    speechRecognition.continuous = true
+    speechRecognition.maxAlternatives = 1
+    speechRecognition.interimResults = true
+
+    speechRecognition.onresult = (event) => {
+       const transcription = Array.from(event.results).reduce((text, result) => {
+          return text.concat(result[0].transcript)
+       }, '') 
+       
+       setContent(transcription)
+    }
+    speechRecognition.onerror = (event) => {
+      console.error(event)
+    }
+    speechRecognition.start()
   }
 
   function handleStopRecording() {
     setIsRecording(false)
+
+    if(speechRecognition !== null) {
+      speechRecognition.stop()
+    }
 
   }
     return (
@@ -53,7 +90,7 @@ export function NewNoteCard({ onNoteCreated} : NewNoteCardProps) {
         </Dialog.Trigger>
         <Dialog.Portal>
           <Dialog.Overlay className="inset-0 fixed bg-black/50" />
-           <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+           <Dialog.Content className="fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] md:w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none">
           <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text text-slate-400 hover:text-slate-100">
             <X className="size-5" />
           </Dialog.Close>
@@ -81,8 +118,9 @@ export function NewNoteCard({ onNoteCreated} : NewNoteCardProps) {
                 <button 
                   type="button" 
                   onClick={handleStopRecording}
-                  className="w-full bg-slate-900 py-4 text-center text-sm text-salte-300 outline-none font-medium hover:text-slate-100"
+                  className="w-full flex items-center justify-center gap-2 bg-slate-900 py-4 text-center text-sm text-salte-300 outline-none font-medium hover:text-slate-100"
                 >
+                  <div className='size-3 rounded-full bg-red-500 animate-pulse' />
                   Gravando! (clique p/ interromper)
                 </button>
              ) : (
